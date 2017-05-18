aa5228058351aa17580020a8e02e3a4ab81c6afe
Aubry <aubry.gudel@axigate.fr>
2017-05-18 15:47:38 +0200

    makeElement function new tool bar with check all, select all and fill all functions Mechanism to keep toolbar usable even when a modal popup is showing

--- axigate/DebugToolbar.js
+++ axigate/DebugToolbar.js
@@ -36,23 +36,84 @@
         }
     }
 
+    class InputTools {
+        static makePanel() {
+            let checkAll, fillAll, selectAll;
+
+            let panel = makeElement('div', {class: "toolBox"}, [
+                    makeElement('input', {
+                        id: 'showHideTools',
+                        type: 'checkbox',
+                        onchange: "document.getElementById('advancedToolBar').style.display = this.checked ? '' : 'none';"
+                    })
+                    , makeElement('label', {for: 'showHideTools'}, 'Show tools')
+                    , makeElement('div', {id: 'advancedToolBar', class: 'oneLine', style: 'display: none'}, [
+                        checkAll = makeElement('div', {
+                            class: 'control',
+                            title: 'Checks all radio button and checkboxes on the page'
+                        }, 'Check all')
+                        , makeElement('input', {type: 'text', id: 'fillingText', placeholder: 'Filling text here'})
+                        , fillAll = makeElement('div', {
+                            class: 'control',
+                            title: 'Fill all text inputs with the specified filling text'
+                        }, 'Fill all')
+                        , makeElement('input', {type: 'number', id: 'dropdownIdx', placeholder: 'Dropdown index'})
+                        , selectAll = makeElement('div', {
+                            class: 'control',
+                            title: 'Selects specified index on all dropdown list'
+                        }, 'Select all')
+                    ])
+                ]
+            );
+
+            checkAll.addEventListener('click', () => {
+                Array.from(document.querySelectorAll("input[type='checkbox']")).filter(x => 'showHideTools' !== x.id).forEach(x => x.click());
+                Array.from(document.querySelectorAll("input[type='radio']")).forEach(x => x.click());
+            });
+
+            fillAll.addEventListener('click', () => {
+                let someText = document.getElementById('fillingText').value;
+                Array.from(document.querySelectorAll("textarea")).forEach(x => x.value = someText);
+                Array.from(document.querySelectorAll("input[type='text']")).forEach(x => x.value = someText);
+            });
+
+            selectAll.addEventListener('click', () => {
+                let idx = document.getElementById('dropdownIdx').value || 1;
+                Array.from(document.querySelectorAll("select")).forEach(x => {
+                    x.selectedIndex = Math.min(idx, x.length)
+                });
+            });
+
+            return panel;
+        }
+    }
+
     const Parameter = jsApi.PlaceControllerApi.Parameter;
+    const controlsAndShortcuts = makeControlsAndShortcuts();
     const mainPanel = injectAndGetMainPanel();
     injectCss();
+    ensureToolBarAlwaysUsable();
 
     function injectAndGetMainPanel() {
         let panel = document.createElement('div');
-        panel.classList.add('oneLine');
         panel.classList.add('ShortCutDoNotCommit');
-        panel.appendChild(makeLightReloadButton());
-        panel.appendChild(makeKeepHereButton());
 
-        PlaceStorage.getStoredPlaces().forEach(place => panel.appendChild(makeGoToButton(place)));
+        panel.appendChild(controlsAndShortcuts);
+        panel.appendChild(InputTools.makePanel());
 
         document.body.appendChild(panel);
         return panel;
-
     }
+
+    function makeControlsAndShortcuts() {
+        let panel = document.createElement('div');
+        panel.classList.add('oneLine');
+        panel.appendChild(makeLightReloadButton());
+        panel.appendChild(makeKeepHereButton());
+        PlaceStorage.getStoredPlaces().forEach(place => panel.appendChild(makeGoToButton(place)));
+        return panel;
+    }
+
 
     function makeKeepHereButton() {
         let keepHere = makeButton('Keep here', 'Add a new shortcut button to current place and arguments');
@@ -63,7 +124,7 @@
                 return;
             }
             let currentPlace = getCurrentPlace();
-            mainPanel.appendChild(makeGoToButton(currentPlace));
+            controlsAndShortcuts.appendChild(makeGoToButton(currentPlace));
             PlaceStorage.storePlace(currentPlace);
         });
 
@@ -71,9 +132,10 @@
     }
 
     function makeGoToButton(place) {
-        let b = makeButton(place.displayName, 'alt+click : current place with stored place params\nshift+click : goto stored place with current params\nctrl+shift+click : delete\nright-click : edit name');
+        let b = makeButton(place.displayName, 'ctrl+shift+click : delete\nright-click : edit name'); //alt+click : current place with stored place params\nshift+click : goto stored place with current params\n
+        b.classList.add('shortcut');
         b.addEventListener('click', e => {
-            if (e.ctrlKey) {
+            if (e.ctrlKey && e.shiftKey) {
                 b.remove();
             } else {
                 goTo(place);
@@ -87,6 +149,25 @@
         });
 
         return b;
+    }
+
+    function makeElement(tag, props = {}, children = []) {
+        let elem = document.createElement(tag);
+        for (const p in props) {
+            elem.setAttribute(p, props[p]);
+        }
+
+        if (!(children instanceof Array)) {
+            children = [children];
+        }
+        for (const child of children) {
+            if (typeof child === 'string' || child instanceof String) {
+                elem.appendChild(document.createTextNode(child));
+            } else {
+                elem.appendChild(child);
+            }
+        }
+        return elem;
     }
 
     function makeButton(text, title) {
@@ -123,6 +204,25 @@
         jsApi.PlaceControllerApi.goTo(place.name, place.parameters);
     }
 
+    function ensureToolBarAlwaysUsable() {
+        new MutationObserver((mutations) => {
+            function isNodeADialog(node) {
+                return 0 !== Array.from(node.classList).filter(c => c.match(/^(gwt-PopupPanel|.+axDialogBox)$/)).length;
+            }
+
+            mutations.forEach((mutation) => {
+                if (0 !== Array.from(mutation.removedNodes).filter(node => isNodeADialog(node)).length) {
+                    mainPanel.remove();
+                    document.body.appendChild(mainPanel);
+                }
+                Array.from(mutation.addedNodes).filter(node => isNodeADialog(node)).forEach(dialog => {
+                    mainPanel.remove();
+                    dialog.appendChild(mainPanel);
+                });
+            });
+        }).observe(document.body, {childList: true});
+    }
+
     function injectCss() {
         let css = document.createElement('style');
         css.appendChild(document.createTextNode(
@@ -130,21 +230,29 @@
                 position: fixed; top: 0px; left: 20px; opacity: 0.2; height: 18px; overflow:hidden; z-index: 9999999;
             }
             .ShortCutDoNotCommit:hover{
-                height: 28px; opacity: 1;
+                height: 80px; opacity: 1;
             }
 
-            .ShortCutDoNotCommit div{
-                padding: 3px 5px; background: white; border: 1px solid grey; cursor: pointer;
+            .shortcut {
+                padding: 3px 5px; 
+                background: white; 
+                border: 1px solid grey; 
+                cursor: pointer;
+            }
+            
+            .control {
+                background-color: #8ab0e5;
+                font-weight: bold;
+                color: white;
+                border: 1px solid #BFD2F3;
+                cursor: pointer;
+                padding: 3px 5px; 
             }
 
             .oneLine {
                 display: flex;
                 align-items: center;
                 flex-wrap: nowrap;
-
-                display: -webkit-flex;
-                -webkit-align-items: center;
-                -webkit-flex-wrap: nowrap
             }
 
             .oneLine > * {
@@ -154,12 +262,16 @@
             .oneLine > :first-child {
                 margin-left: 0;
             }
-
-            .ShortCutDoNotCommit div.control {
-                background-color: #8ab0e5;
-                font-weight: big;
-                color: white;
-                border-color: #BFD2F3;
+            
+            .toolBox{
+                background-color: rgba(8, 31, 51, 0.7);
+                color: lightgrey;
+                padding: 4px;
+                border-radius: 5px;
+            }
+            
+            .gwt-PopupPanelGlass{
+                pointer-events: none
             }
 `
         ));
